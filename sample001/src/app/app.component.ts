428addd39dfbbdd27ef70eb7149b3da73efd2e28
Ndricim Rrapi <ndricim.rrapi@sap.com>
2021-09-23 10:32:55 +0200

    Add changes

---
+++ sample001/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  open = true;
+
+  openCloseModal() {
+    this.open = !this.open; 
+  }
+}
